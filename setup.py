97aee194aa40df8fe2c7930e36503d26d327a568
KingJem <KingJem@users.noreply.github.com>
2024-11-25 03:12:21 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for vidmate package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("vidmate", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="vidmate",
+    version=read("vidmate", "VERSION"),
+    description="Awesome vidmate created by KingJem",
+    url="https://github.com/KingJem/VidMate/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="KingJem",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["vidmate = vidmate.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
